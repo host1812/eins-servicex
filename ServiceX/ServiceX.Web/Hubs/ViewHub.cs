ab8c6b5a2d33126354b7d77c3ea99d7c8e8a7f74
afedorenchik <afedorenchik@microsoft.com>
2024-02-07 18:15:58 -0500

    a simple controller

---
+++ ServiceX/ServiceX.Web/Hubs/ViewHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ServiceX.Web.Hubs;
+
+public class ViewHub : Hub
+{
+    public static int ViewCount { get; private set; } = 0;
+    public async Task NotifyWatching()
+    {
+        ViewCount++;
+        await Clients.All.SendAsync("ViewCountUpdate", ViewCount);
+    }
+}
