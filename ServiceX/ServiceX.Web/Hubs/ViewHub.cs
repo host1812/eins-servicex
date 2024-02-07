3ed734f5cd3b5fce09bb37eaf8eca43f456e7f28
afedorenchik <afedorenchik@microsoft.com>
2024-02-07 18:42:25 -0500

    a very basic signalR example

--- ServiceX/ServiceX.Web/Hubs/ViewHub.cs
+++ ServiceX/ServiceX.Web/Hubs/ViewHub.cs
@@ -4,9 +4,17 @@ namespace ServiceX.Web.Hubs;
 
 public class ViewHub : Hub
 {
+    private ILogger<ViewHub> _logger;
+
+    public ViewHub(ILogger<ViewHub> logger)
+    {
+        _logger = logger;
+    }
+
     public static int ViewCount { get; private set; } = 0;
     public async Task NotifyWatching()
     {
+        _logger.LogInformation($"View Count Updated: {ViewCount}");
         ViewCount++;
         await Clients.All.SendAsync("ViewCountUpdate", ViewCount);
     }
