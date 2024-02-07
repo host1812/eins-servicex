df8f19f370b701192a7261d533f833b9cf3cc97a
afedorenchik <afedorenchik@microsoft.com>
2024-02-07 18:55:40 -0500

    missing hub js and ts

---
+++ ServiceX/ServiceX.Web/wwwroot/hubs/viewhub.js
@@ -0,0 +1,27 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder()
+  .configureLogging(signalR.LogLevel.None)
+  .withUrl("/hubs/view")
+  .build();
+
+connection.on("ViewCountUpdate", (viewCount) => {
+  console.log(viewCount);
+  var counter = document.getElementById("viewCounter");
+  counter.innerText = viewCount;
+});
+
+function notify() {
+  connection.send("NotifyWatching");
+}
+
+function startSuccess() {
+  console.log("Connected to hub");
+  notify();
+}
+
+function startFail() {
+  console.log("Failed to connect to hub");
+}
+
+connection.start().then(startSuccess, startFail);
---
+++ ServiceX/ServiceX.Web/wwwroot/hubs/viewhub.ts
@@ -0,0 +1,27 @@
+﻿import * as signalR from "../lib/microsoft/signalr/dist/browser/signalr";
+
+let connection = new signalR.HubConnectionBuilder().withUrl("/hubs/view").build();
+
+connection.on("ReceiveMessage", (user, message) => {
+    console.log(`Received message from ${user}: ${message}`);
+});
+
+function notify() {
+    connection.send("NotifyWatching");
+}
+
+function startSuccess() {
+    console.log("Connection started");
+    notify();
+}
+
+function startFail(err) {
+    console.error(err.toString());
+}
+
+connection.on("ViewCountUpdate", (viewCount: number) => {
+    var counter = document.getElementById("viewCount");
+    counter.innerText = viewCount.toString();
+})
+
+connection.start().then(startSuccess, startFail);
